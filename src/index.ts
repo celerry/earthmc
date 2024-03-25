3cd110237a8d7023ed7a4adfcddc0c542334a2bf
kokiriglade <60290002+celerry@users.noreply.github.com>
2024-03-25 20:30:56 +0000

    add example to earthmc class itself

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,15 @@ import { URL } from "node:url"
 
 /**
  * Used to interact with the EarthMC API
+ * 
+ * ```ts
+ * import { EarthMC } from "@celery/earthmc";
+ *
+ * const api = new EarthMC()
+ *
+ * const players = await api.players() // [{ uuid:  ... , name: ... }, ...]
+ * const town = await api.town(players[0].town.uuid) [{ board: ... , ... }]
+ * ```
  */
 export class EarthMC {
     /**
