77924d6f3284afd28d352d7c06b9c93cdf054692
kokiriglade <60290002+celerry@users.noreply.github.com>
2024-03-25 20:29:49 +0000

    some more doc stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import { URL } from "node:url"
 
+/**
+ * This modules contains the core of the wrapper
+ * @module
+ * 
+ * ```ts
+ * import { EarthMC } from "@celery/earthmc";
+ *
+ * const api = new EarthMC()
+ *
+ * const players = await api.players() // [{ uuid:  ... , name: ... }, ...]
+ * const town = await api.town(players[0].town.uuid) [{ board: ... , ... }]
+ * ```
+ */
+
+/**
+ * Used to interact with the EarthMC API
+ */
 export class EarthMC {
     /**
      * The base URL of the EarthMC API
